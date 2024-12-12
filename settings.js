4545a2fb9e002e3d50fa7edd66dae107df3ad30d
Disco <4949083+the-disco-option@users.noreply.github.com>
2024-12-12 16:10:27 +0000

    declutter

--- settings.js
+++ settings.js
@@ -34,10 +34,7 @@ class Modification {
 }
 
 export let MODIFICATIONS = new Map([
-    ["2-0-10", new Modification("Vanilla 2.0.10", "vanilla-2.0.10.json", false)],
-    ["1-1-110", new Modification("Vanilla 1.1.110", "vanilla-1.1.110.json", true)],
-    ["1-1-110x", new Modification("Vanilla 1.1.110 - Expensive", "vanilla-1.1.110-expensive.json", true)],
-    ["space-age-2-0-11", new Modification("Space Age 2.0.11 (WORK IN PROGRESS)", "space-age-2.0.11.json", false)],
+    ["2-0-10", new Modification("Alpha 2 Phase 1", "vanilla-2.0.10.json", false)],
 ])
 
 let DEFAULT_MODIFICATION = "2-0-10"
