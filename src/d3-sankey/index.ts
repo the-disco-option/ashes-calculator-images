29ef08cba2b5a6aa159bdcd39063a0cc879231da
Disco <4949083+the-disco-option@users.noreply.github.com>
2025-04-26 22:14:59 +0200

    Delete src directory

--- src/d3-sankey/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as sankey } from './sankey'
-export {
-  center as sankeyCenter,
-  left as sankeyLeft,
-  right as sankeyRight,
-  justify as sankeyJustify,
-} from './align'
-export { default as sankeyLinkHorizontal } from './sankeyLinkHorizontal'
